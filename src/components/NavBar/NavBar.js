9a03f1e7e0af30bfd11d59902efdbd0a4f063232
annadrg <annaderuig@gmail.com>
2021-02-26 15:55:47 +0100

    Added Navbar and About us page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,29 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.scss";
 
-const NavBar = () => {
+export default function NavBar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="Navbar">
+      <div className="link">
+        <NavLink
+          className="inactive"
+          activeClassName="active"
+          exact={true}
+          to="/"
+        >
+          Home
+        </NavLink>
+      </div>
+      <div className="link">
+        <NavLink
+          className="inactive"
+          activeClassName="active"
+          exact={true}
+          to="/about"
+        >
+          About
+        </NavLink>
+      </div>
     </div>
   );
-};
-
-export default NavBar;
+}
---
+++ src/pages/AboutUsPage/AboutUsPage.js
@@ -0,0 +1,8 @@
+export default function AboutUsPage() {
+  return (
+    <div className="aboutUs">
+      <h2>About us</h2>
+      <p>Welcome to our article app!</p>
+    </div>
+  );
+}
